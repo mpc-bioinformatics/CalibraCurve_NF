04baaf764390f917f227f48846b59ba62e6a5c02
KarinSchork <karin.schork@rub.de>
2025-06-16 15:42:28 +0000

    add further parameters

--- bin/Run_CalibraCurve.R
+++ bin/Run_CalibraCurve.R
@@ -2,6 +2,10 @@
 
 # first install packages affy, matrixStats and vsn
 #install.packages("../ProtStatsWF", repos = NULL, type="source")
+
+### specify paths to package library, otherwise the package will not be found
+.libPaths("/home/ubuntu/R/x86_64-pc-linux-gnu-library/4.5")
+
 library(CalibraCurve)
 library(optparse)
 
@@ -19,7 +23,99 @@ option_list <- list(
   make_option(opt_str = c("--meas_col"), 
               type = "integer",
               #default = 2,
-              help = "Column number of the measurement column in the data.")
+              help = "Column number of the measurement column in the data."),
+  make_option(opt_str = c("--substance"), 
+              type = "character",
+              default = "",
+              help = ""),
+  make_option(opt_str = c("--suffix"), 
+              type = "character",
+              default = "",
+              help = ""),
+  make_option(opt_str = c("--min_replicates"),
+              type = "integer",
+              default = 3,
+              help = "Minimum number of replicates for a concentration level to be included in the calibration curve. Default is 3."),
+  make_option(opt_str = c("--cv_thres"),
+              type = "numeric",
+              default = 20,
+              help = "Threshold for CV per concentration level in percent (default is 20)."),
+  make_option(opt_str = c("--calcContinuousPrelimRanges"),
+              type = "logical",
+              default = TRUE,
+              help = "If TRUE, the longest continuous range is selected (default is TRUE). If FALSE, gaps with CVs larger than the threshold may be included."),
+  make_option(opt_str = c("--weightingMethod"),
+              type = "character",
+              default = "1/x^2",
+              help = "Method for weighting (currently 1/x, 1/x^2 and None are supported, default is 1/x^2)."),
+  make_option(opt_str = c("--centralTendencyMeasure"),
+              type = "character",
+              default = "mean",
+              help = "Method for calculating average percent bias, mean (default) or median."),
+  make_option(opt_str = c("--perBiasThres"),
+              type = "numeric",
+              default = 20,
+              help = "Threshold for average percent bias in percent, default is 20."),
+  make_option(opt_str = c("--considerPerBiasCV"),
+              type = "logical",
+              default = TRUE,
+              help = "If TRUE, CV is considered for the elimination of the concentration level (default). CV will only be considered if the difference in percent bias values is lower than perBiasDistThres."),
+  make_option(opt_str = c("--perBiasDistThres"),
+              type = "numeric",
+              default = 10,
+              help = "Threshold for the difference in average percent bias in percent (for lower differences, CV will be considered), default is 10."),
+  make_option(opt_str = c("--RfThresL"),
+              type = "numeric",
+              default = 80,
+              help = "Lower threshold for response factor in percent (default is 80)."),
+  make_option(opt_str = c("--RfThresU"),
+              type = "numeric",
+              default = 120,
+              help = "Upper threshold for response factor in percent (default is 120)."), 
+  make_option(opt_str = c("--xlab"), 
+              type = "character",
+              default = "Concentration",
+              help = "Label of the x-axis"),
+  make_option(opt_str = c("--ylab"), 
+              type = "character",
+              default = "Intensity",
+              help = "Label of the y-axis"),
+  make_option(opt_str = c("--show_regression_info"),
+              type = "logical",
+              default = FALSE,
+              help = "If TRUE, show regression information (R2, slope, intercept) on the plot."),
+  make_option(opt_str = c("--show_linear_range"),
+              type = "logical",
+              default = TRUE,
+              help = "If TRUE, show the linear range of the calibration curve as a rectangle in the plot."),
+  make_option(opt_str = c("--show_data_points"),
+              type = "logical",
+              default = TRUE,
+              help = "If TRUE, show the data points on the plot."),
+  make_option(opt_str = c("--point_colour"), 
+              type = "character",
+              default = "black",
+              help = "Colour for the data points in the plot."),
+  make_option(opt_str = c("--curve_colour"), 
+              type = "character",
+              default = "red",
+              help = "Colour for the calibration curve in the plot."),
+  make_option(opt_str = c("--linear_range_colour"), 
+              type = "character",
+              default = "black",
+              help = "Colour for the linear range rectangle in the plot."),
+  make_option(opt_str = c("--RF_colour_threshold"), 
+              type = "character",
+              default = "orange",
+              help = "Label of the y-axis"),
+  make_option(opt_str = c("--RF_colour_within"), 
+              type = "character",
+              default = "#00BFC4",
+              help = "Label of the y-axis"),
+  make_option(opt_str = c("--RF_colour_outside"), 
+              type = "character",
+              default = "#F8766D",
+              help = "Label of the y-axis")  
 )
 
 #print(option_list)
@@ -35,13 +131,48 @@ if(is.null(opt$output_path)){
 if(is.null(opt$conc_col)){
   message("Column number of concentration column is missing. Add: --conc_col <number>")
 }
-if(is.null(opt$mea_col)){
+if(is.null(opt$meas_col)){
   message("Column number of measurement column is missing. Add: --meas_col <number>")
 }
 
-print(opt)
+if(opt$substance == "_"){
+  dir <- opt$data_path
+  opt$substance <- strsplit(basename(dir), "\\.")[[1]][1]
+  message("Substance name is missing. Using the name of the data folder: ", opt$substance)
+}
+if(opt$suffix == "_"){
+  opt$suffix <- paste0("_", opt$substance)
+  message("Suffix is missing. Using substance name.")
+}
+
 
 CalibraCurve(data_path = opt$data_path,
-                    output_path = opt$output_path,
-                    conc_col = opt$conc_col,
-                    meas_col = opt$meas_col)
+             output_path = opt$output_path,
+             conc_col = opt$conc_col,
+             meas_col = opt$meas_col, 
+             substance = opt$substance,
+             suffix = opt$suffix,
+             min_replicates = opt$min_replicates,
+             cv_thres = opt$cv_thres,
+             calcContinuousPrelimRanges = opt$calcContinuousPrelimRanges,
+             weightingMethod = opt$weightingMethod,
+             centralTendencyMeasure = opt$centralTendencyMeasure,
+             perBiasThres = opt$perBiasThres,
+             considerPerBiasCV = opt$considerPerBiasCV,
+             perBiasDistThres = opt$perBiasDistThres,
+             RfThresL = opt$RfThresL,
+             RfThresU = opt$RfThresU, 
+             xlab = opt$xlab,
+             ylab = opt$ylab,
+             show_regression_info = opt$show_regression_info,
+             show_linear_range = opt$show_linear_range,
+             show_data_points = opt$show_data_points,
+             point_colour = opt$point_colour,
+             curve_colour = opt$curve_colour,
+             linear_range_colour = opt$linear_range_colour,
+             RF_colour_threshold = opt$RF_colour_threshold,
+             RF_colour_within = opt$RF_colour_within,
+             RF_colour_outside = opt$RF_colour_outside)
+
+
+
--- main_CalibraCurve.nf
+++ main_CalibraCurve.nf
@@ -1,10 +1,33 @@
 #!usr/bin/env nextflow
 nextflow.enable.dsl=2
 
-params.data_path = "//mnt/data/Documents/CalibraCurve_NF/testdata/ALB_LVNEVTEFAK_y8.xlsx"
 params.output_path = "."
-params.conc_col = 6
-params.meas_col = 7
+params.conc_col = 1
+params.meas_col = 2
+params.substance = "_"
+params.suffix = "_"
+params.min_replicates = 3
+params.cv_thres = 20
+params.calcContinuousPrelimRanges = true
+params.weightingMethod = "1/x^2"
+params.centralTendencyMeasure = "mean"
+params.perBiasThres = 20
+params.considerPerBiasCV = true
+params.perBiasDistThres = 10
+params.RfThresL = 80
+params.RfThresU = 120
+params.xlab = "Concentration"
+params.ylab = "Intensity"
+params.show_regression_info = false
+params.show_linear_range = true
+params.show_data_points = true
+params.point_colour = "black"
+params.curve_colour = "red"
+params.linear_range_colour = "black"
+params.RF_colour_threshold = "orange"
+params.RF_colour_within = "#00BFC4"
+params.RF_colour_outside = "#F8766D"
+
 
 process Rscript {
   container 'mpc/calibracurve-r:1.0.0'
@@ -19,6 +42,29 @@ process Rscript {
     val output_path
     val conc_col
     val meas_col
+    val substance
+    val suffix
+    val min_replicates
+    val cv_thres
+    val calcContinuousPrelimRanges
+    val weightingMethod
+    val centralTendencyMeasure
+    val perBiasThres
+    val considerPerBiasCV
+    val perBiasDistThres
+    val RfThresL
+    val RfThresU
+    val xlab
+    val ylab
+    val show_regression_info
+    val show_linear_range
+    val show_data_points
+    val point_colour
+    val curve_colour
+    val linear_range_colour
+    val RF_colour_threshold
+    val RF_colour_within
+    val RF_colour_outside
 
   output:
     path("*.png")
@@ -26,11 +72,21 @@ process Rscript {
     path("*.xlsx")
 
   """
-  Run_CalibraCurve.R --data_path ${data_path} --output_path "." --conc_col ${conc_col} --meas_col ${meas_col}
+  Run_CalibraCurve.R --data_path ${data_path} --output_path "." --conc_col ${conc_col} --meas_col ${meas_col} --substance ${substance} --suffix ${suffix} --min_replicates ${min_replicates} --cv_thres ${cv_thres} --calcContinuousPrelimRanges ${calcContinuousPrelimRanges} --weightingMethod ${weightingMethod} --centralTendencyMeasure ${centralTendencyMeasure} --perBiasThres ${perBiasThres} --considerPerBiasCV ${considerPerBiasCV} --perBiasDistThres ${perBiasDistThres} --RfThresL ${RfThresL} --RfThresU ${RfThresU} --xlab "${xlab}" --ylab "${ylab}" --show_regression_info ${show_regression_info} --show_linear_range ${show_linear_range} --show_data_points ${show_data_points} --point_colour "${point_colour}" --curve_colour "${curve_colour}" --linear_range_colour "${linear_range_colour}" --RF_colour_threshold "${RF_colour_threshold}" --RF_colour_within "${RF_colour_within}" --RF_colour_outside "${RF_colour_outside}"
   """
 }
 
 workflow {
-  Rscript(params.data_path, params.output_path, params.conc_col, params.meas_col) 
+  Rscript(params.data_path, params.output_path, params.conc_col, params.meas_col, 
+          params.substance, params.suffix,
+          params.min_replicates, params.cv_thres, params.calcContinuousPrelimRanges, 
+          params.weightingMethod, params.centralTendencyMeasure, params.perBiasThres, 
+          params.considerPerBiasCV, params.perBiasDistThres, params.RfThresL, 
+          params.RfThresU,  params.xlab, params.ylab,
+          params.show_regression_info, params.show_linear_range,
+          params.show_data_points, params.point_colour,
+          params.curve_colour, params.linear_range_colour,
+          params.RF_colour_threshold, params.RF_colour_within,
+          params.RF_colour_outside)
 }
 
